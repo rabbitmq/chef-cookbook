f90f02f5fb6a6469ffa96a8a80a0d1a6f1af32ab
Tom Howe <turtlebender@gmail.com>
2012-08-20 10:51:36 -0500

    fixing missing output > in cluster recipe

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -7,12 +7,12 @@ disk_nodes = Set.new disk_nodes
 
 disk_node_string = "rabbit@" + disk_nodes.map { |node| node.name }.join(" rabbit@")
 if disk_node_string != "rabbit@#{node.name}"
-  `rabbitmqctl cluster_status /dev/null 2>&1`
+  `rabbitmqctl cluster_status > /dev/null 2>&1`
 
   if $? != 0
 	  execute "rabbitmqctl stop_app"
 	  execute "rabbitmqctl reset"
 	  execute "rabbitmqctl cluster #{disk_node_string}"
-  	  execute "rabbitmqctl start_app"
+	  execute "rabbitmqctl start_app"
   end
 end
