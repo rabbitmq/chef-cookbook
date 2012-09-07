5e8b8dddc150c1e1b66b7e2e8e4f868c5d17625a
Darren Birkett <darren.birkett@gmail.com>
2012-09-07 11:01:06 +0100

    fully parameterize rabbitmq-server version for use with yum
    * add ['rabbitmq']['arch'] attribute
    * add ['rabbitmq']['release'] attribute
    * update server recipe to use new attributes
    * update recipe to use yum_package to allow use of arch attribute

--- recipes/default.rb
+++ recipes/default.rb
@@ -55,17 +55,18 @@ when "debian", "ubuntu"
 when "redhat", "centos", "scientific", "amazon", "fedora"
 
   if node['rabbitmq']['use_yum'] then
-    package "rabbitmq-server" do
-      version "#{node['rabbitmq']['version']}-1"
+    yum_package "rabbitmq-server" do
+      version "#{node['rabbitmq']['version']}"-"#{node['rabbitmq']['release']}"
+      arch "#{node['rabbitmq']['arch']}"
       action :install
     end
   else
-    remote_file "#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{node['rabbitmq']['version']}-1.noarch.rpm" do
-      source "https://www.rabbitmq.com/releases/rabbitmq-server/v#{node['rabbitmq']['version']}/rabbitmq-server-#{node['rabbitmq']['version']}-1.noarch.rpm"
+    remote_file "#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{node['rabbitmq']['version']}-#{node['rabbitmq']['release']}.#{node['rabbitmq']['arch']}.rpm" do
+      source "https://www.rabbitmq.com/releases/rabbitmq-server/v#{node['rabbitmq']['version']}/rabbitmq-server-#{node['rabbitmq']['version']}-#{node['rabbitmq']['release']}.#{node['rabbitmq']['arch']}.rpm"
       action :create_if_missing
     end
 
-    rpm_package "#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{node['rabbitmq']['version']}-1.noarch.rpm" do
+    rpm_package "#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{node['rabbitmq']['version']}-#{node['rabbitmq']['release']}.#{node['rabbitmq']['arch']}.rpm" do
       action :install
     end
   end
