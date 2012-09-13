7cc29521a3c3f0a1abd9c0934f3f4b9f59cfd4ec
Tom Howe <turtlebender@gmail.com>
2012-09-13 09:34:03 -0500

    check for presence of .reset_mnesia_database before resetting for cluster

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -1,18 +1,18 @@
-disk_nodes = search(:node, "role:rabbitdisknode")
+unless File.exists?('/var/lib/rabbitmq/.reset_mnesia_database')
+	disk_nodes = search(:node, "role:rabbitdisknode AND chef_environment:#{node.chef_environment}")
+	unless disk_nodes.empty? || disk_nodes.nil?
+		disk_nodes = Set.new disk_nodes
 
-if disk_nodes.empty? || disk_nodes.nil?
-	disk_nodes = Array.new
-end
-disk_nodes = Set.new disk_nodes
+		disk_node_string = "rabbit@" + disk_nodes.map { |node| node.name }.join(" rabbit@")
+		if disk_node_string != "rabbit@#{node.name}"
+			`rabbitmqctl cluster_status > /dev/null 2>&1`
 
-disk_node_string = "rabbit@" + disk_nodes.map { |node| node.name }.join(" rabbit@")
-if disk_node_string != "rabbit@#{node.name}"
-  `rabbitmqctl cluster_status > /dev/null 2>&1`
-
-  if $? != 0
-	  execute "rabbitmqctl stop_app"
-	  execute "rabbitmqctl reset"
-	  execute "rabbitmqctl cluster #{disk_node_string}"
-	  execute "rabbitmqctl start_app"
-  end
+			if $? != 0
+				execute "rabbitmqctl stop_app"
+				execute "rabbitmqctl reset"
+				execute "rabbitmqctl cluster #{disk_node_string}"
+				execute "rabbitmqctl start_app"
+			end
+		end
+	end
 end
