fc254b5229912d8aa5cdfb609d398558bed3d883
Dan Buch <d.buch@modcloth.com>
2012-11-11 20:25:07 -0500

    Bumping minor version and adding smartos to list of supported platforms
    even though I've done nothing yet to make it a reality.

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ depends           "apt", ">= 1.4.4"
 depends           "yum", ">= 0.5.0"
 depends           "erlang", ">= 0.9"
 
-%w{ubuntu debian redhat centos scientific amazon fedora oracle}.each do |os|
+%w{ubuntu debian redhat centos scientific amazon fedora oracle smartos}.each do |os|
   supports os
 end
 
