8794a48d1434af7aab1f2c340b49b6d4583d2634
Dan Buch <d.buch@modcloth.com>
2012-11-30 19:46:56 -0500

    Adjusting attributes per feedback on [COOK-1888]

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,6 @@ default['rabbitmq']['logdir'] = nil
 default['rabbitmq']['mnesiadir'] = nil
 
 default['rabbitmq']['service_name'] = 'rabbitmq-server'
-if platform?('smartos')
-  default['rabbitmq']['service_name'] = 'rabbitmq'
-end
 
 # RabbitMQ version to install for "redhat", "centos", "scientific", and "amazon".
 default['rabbitmq']['version'] = '2.8.4'
@@ -25,14 +22,7 @@ default['rabbitmq']['use_distro_version'] = false
 # "The .config extension is automatically appended by the Erlang runtime."
 default['rabbitmq']['config_root'] = "/etc/rabbitmq"
 default['rabbitmq']['config'] = "/etc/rabbitmq/rabbitmq"
-if platform?('smartos')
-  default['rabbitmq']['config_root'] = "/opt/local/etc/rabbitmq"
-  default['rabbitmq']['config'] = "/opt/local/etc/rabbitmq/rabbitmq"
-end
 default['rabbitmq']['erlang_cookie_path'] = '/var/lib/rabbitmq/.erlang.cookie'
-if platform?('smartos')
-  default['rabbitmq']['erlang_cookie_path'] = '/var/db/rabbitmq/.erlang.cookie'
-end
 
 # rabbitmq.config defaults
 default['rabbitmq']['default_user'] = 'guest'
@@ -49,3 +39,11 @@ default['rabbitmq']['ssl_port'] = 5671
 default['rabbitmq']['ssl_cacert'] = '/path/to/cacert.pem'
 default['rabbitmq']['ssl_cert'] = '/path/to/cert.pem'
 default['rabbitmq']['ssl_key'] = '/path/to/key.pem'
+
+# SmartOS-specific defaults
+if node[:platform] == 'smartos'
+  default['rabbitmq']['service_name'] = 'rabbitmq'
+  default['rabbitmq']['config_root'] = '/opt/local/etc/rabbitmq'
+  default['rabbitmq']['config'] = '/opt/local/etc/rabbitmq/rabbitmq'
+  default['rabbitmq']['erlang_cookie_path'] = '/var/db/rabbitmq/.erlang.cookie'
+end
