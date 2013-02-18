f3c02d681652c6c879411488e649adea19a30038
Sean Porter <portertech@gmail.com>
2013-02-17 18:59:14 -0800

    [path] chef execute resource "path" must be an array

--- providers/plugin.rb
+++ providers/plugin.rb
@@ -17,8 +17,9 @@
 # limitations under the License.
 #
 
-def plugins_bin_path
-  ENV.fetch('PATH') + ':/usr/lib/rabbitmq/bin'
+def plugins_bin_path(return_array=false)
+  path = ENV.fetch('PATH') + ':/usr/lib/rabbitmq/bin'
+  return_array ? path.split(':') : path
 end
 
 def plugin_enabled?(name)
@@ -34,7 +35,7 @@ action :enable do
   unless plugin_enabled?(new_resource.plugin)
     execute "rabbitmq-plugins enable #{new_resource.plugin}" do
       Chef::Log.info "Enabling RabbitMQ plugin '#{new_resource.plugin}'."
-      path plugins_bin_path
+      path plugins_bin_path(true)
       new_resource.updated_by_last_action(true)
     end
   end
@@ -44,7 +45,7 @@ action :disable do
   if plugin_enabled?(new_resource.plugin)
     execute "rabbitmq-plugins disable #{new_resource.plugin}" do
       Chef::Log.info "Disabling RabbitMQ plugin '#{new_resource.plugin}'."
-      path plugins_bin_path
+      path plugins_bin_path(true)
       new_resource.updated_by_last_action(true)
     end
   end
