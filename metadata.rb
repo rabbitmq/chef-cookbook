1cd99f4094bc17398caf837046a04dc07dd17d04
Dorian Jaminais <dorian.jaminais@perfect-memory.com>
2013-05-07 13:35:06 +0200

    added support to install packages from upstream debian repo

--- metadata.rb
+++ metadata.rb
@@ -10,6 +10,7 @@ recipe            "rabbitmq::plugin_management", "Manage plugins with node attri
 recipe            "rabbitmq::virtualhost_management", "Manage virtualhost with node attributes"
 recipe            "rabbitmq::user_management", "Manage users with node attributes"
 depends           "erlang", ">= 0.9"
+depends           "apt", ">= 1.9"
 
 %w{ubuntu debian linuxmint redhat centos scientific amazon fedora oracle smartos}.each do |os|
   supports os
