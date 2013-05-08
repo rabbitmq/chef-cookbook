ae7954bd36bcf71005473a8a850e94d435ce256b
Kiall Mac Innes <kiall@managedit.ie>
2013-05-08 13:40:41 +0100

    Add policy_management recipe

---
+++ recipes/policy_management.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: rabbitmq
+# Recipe:: policy_management
+#
+# Copyright 2013, Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "rabbitmq::default"
+include_recipe "rabbitmq::virtualhost_management"
+
+policies = node['rabbitmq']['policies']
+
+policies.each do |policy|
+  rabbitmq_policy policy['name'] do
+    pattern     policy['pattern']
+    definition  policy['definition']
+    priority    policy.fetch('priority', 10)
+    vhost       policy.fetch('vhost', '/')
+    action     :set
+  end
+end
