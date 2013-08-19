82b50c5687f58455cdee376205b13d9b11886ccd
Matt Ray <matthewhray@gmail.com>
2013-08-19 17:01:39 -0500

    distro version won't pass service and connection tests, but does work

--- test/cookbooks/rabbitmq_test/files/default/tests/minitest/default_test.rb
+++ test/cookbooks/rabbitmq_test/files/default/tests/minitest/default_test.rb
@@ -16,26 +16,9 @@
 
 require File.expand_path('../support/helpers', __FILE__)
 
+
 describe "rabbitmq_test::default" do
   include Helpers::RabbitMQ
-
-  # it 'uses the rabbitmq apt source on debian family' do
-  #   unless node['platform_family'] == 'debian'
-  #     skip "Only applicable on Debian family"
-  #   end
-
-  #   file("/etc/apt/sources.list.d/rabbitmq-source.list").must_exist
-  # end
-
-  # it 'installs the package from downloaded rpm on rhel/fedora family' do
-  #   unless node['platform_family'] == 'rhel' || node['platform_family'] == 'fedora'
-  #     skip "Only applicable on RHEL/Fedora family"
-  #   end
-
-  #   rpm_path = "#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{node['rabbitmq']['version']}-1.noarch.rpm"
-
-  #   file(rpm_path).must_exist
-  # end
 
   #packages
   it 'installs the rabbitmq-server package' do
@@ -60,17 +43,19 @@ describe "rabbitmq_test::default" do
   # service
   it 'enables & starts the rabbitmq-server service' do
     service(node['rabbitmq']['service_name']).must_be_enabled unless node['rabbitmq']['job_control'] == 'upstart'
-    service(node['rabbitmq']['service_name']).must_be_running
+    service(node['rabbitmq']['service_name']).must_be_running unless node['rabbitmq']['use_distro_version']
   end
 
   # accepts connections
   it 'accepts AMQP connections' do
-    require 'bunny'
-    b = Bunny.new( :host => "localhost",
-                   :port => 5672,
-                   :user => node['rabbitmq']['default_user'],
-                   :pass => node['rabbitmq']['default_pass'] )
-    b.start
-    b.stop
+    unless node['rabbitmq']['use_distro_version']
+      require 'bunny'
+      b = Bunny.new( :host => "localhost",
+        :port => 5672,
+        :user => node['rabbitmq']['default_user'],
+        :pass => node['rabbitmq']['default_pass'] )
+      b.start
+      b.stop
+    end
   end
 end
