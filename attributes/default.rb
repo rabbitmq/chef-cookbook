46050139daa1b1c1a36853181cf9fa94e6671ade
Lianping <lianping.chen@paddypower.com>
2013-09-06 12:14:47 +0100

    Made web console ssl port configurable.

--- attributes/default.rb
+++ attributes/default.rb
@@ -52,6 +52,7 @@ default['rabbitmq']['ssl_key'] = '/path/to/key.pem'
 default['rabbitmq']['ssl_verify'] = 'verify_none'
 default['rabbitmq']['ssl_fail_if_no_peer_cert'] = false
 default['rabbitmq']['web_console_ssl'] = false
+default['rabbitmq']['web_console_ssl_port'] = 15671
 
 #tcp listen options
 default['rabbitmq']['tcp_listen_packet'] = 'raw'
