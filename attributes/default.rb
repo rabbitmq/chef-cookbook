a82bc3072f53ad064d6a71cd6d5cf002fddcaa9d
Philip Champon <philip@adaptly.com>
2014-02-07 12:42:28 -0500

    added

--- attributes/default.rb
+++ attributes/default.rb
@@ -90,6 +90,9 @@ when 'smartos'
   default['rabbitmq']['erlang_cookie_path'] = '/var/db/rabbitmq/.erlang.cookie'
 end
 
+# heartbeat
+default['rabbitmq']['heartbeat'] = 600
+
 # Example HA policies
 default['rabbitmq']['policies']['ha-all']['pattern'] = "^(?!amq\\.).*"
 default['rabbitmq']['policies']['ha-all']['params'] = { "ha-mode" => "all" }
