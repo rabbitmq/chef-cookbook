fa196e7abcc2593fea229b224e4923d4451e31aa
Andrew G. Robson <arobson@crittercism.com>
2014-07-25 14:37:38 -0700

    Changes to allow loopback_users to be specified

--- attributes/default.rb
+++ attributes/default.rb
@@ -75,6 +75,7 @@ default['rabbitmq']['enabled_users'] =
     [{ :vhost => nil , :conf => '.*', :write => '.*', :read => '.*' }]
   }]
 default['rabbitmq']['disabled_users'] = []
+default['rabbitmq']['loopback_users'] = '[<<"guest">>]'
 
 # plugins
 default['rabbitmq']['enabled_plugins'] = []
