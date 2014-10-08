dc41f8cf71e98590e760aa9acd4a89f40af41ad6
Mark Vanderwiel <vanderwl@us.ibm.com>
2014-10-08 09:32:02 -0500

    Update user.rb

--- providers/user.rb
+++ providers/user.rb
@@ -85,6 +85,7 @@ action :add do
     new_password = new_resource.password.gsub("'", "'\\\\''")
     cmd = "rabbitmqctl add_user #{new_resource.user} '#{new_password}'"
     execute "rabbitmqctl add_user #{new_resource.user}" do
+      sensitive true
       command cmd
       Chef::Log.info "Adding RabbitMQ user '#{new_resource.user}'."
     end
@@ -156,6 +157,7 @@ action :change_password do
   if user_exists?(new_resource.user)
     cmd = "rabbitmqctl change_password #{new_resource.user} #{new_resource.password}"
     execute cmd do
+      sensitive true
       Chef::Log.debug "rabbitmq_user_change_password: #{cmd}"
       Chef::Log.info "Editing RabbitMQ user '#{new_resource.user}'."
     end
