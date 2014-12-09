55f679c4c6d364555316a6247ebb29bbd9257b40
Daniel Galinkin <daniel.galinkin@zahpee.com>
2014-12-08 23:36:36 -0200

    Default spec for the community_plugins recipe

---
+++ spec/community_plugins.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'rabbitmq::community_plugins' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new.converge(described_recipe)
+  end
+
+  let(:file_cache_path) { Chef::Config[:file_cache_path] }
+
+  it 'includes the `default` recipe' do
+    expect(chef_run).to include_recipe('rabbitmq::default')
+  end
+end
