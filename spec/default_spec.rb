e8090ea076c9b82b834698961459300a2cb91cae
JJ Asghar <jj@getchef.com>
2014-12-15 15:13:21 -0600

    Added Three different os supports
    - Added Redhat chefspec support
    - Added Ubuntu chefspec support
    - Added Suse chefspec support
    - Also added a couple other specs for logrotate
    - Removed the hard coding for 3.4.2-1

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'rabbitmq::default' do
 
-  version = '3.4.2-1'
-
+  let(:runner) { ChefSpec::ServerRunner.new(REDHAT_OPTS) }
+  let(:node) { runner.node }
   let(:chef_run) do
-    ChefSpec::ServerRunner.new.converge(described_recipe)
+    runner.converge(described_recipe)
   end
 
   let(:file_cache_path) { Chef::Config[:file_cache_path] }
@@ -38,12 +38,82 @@ describe 'rabbitmq::default' do
     expect(chef_run).to start_service('rabbitmq-server')
   end
 
+
+  it 'should install the erlang package' do
+    expect(chef_run).to install_package('erlang')
+  end
+
+  describe 'suse' do
+    let(:runner) { ChefSpec::ServerRunner.new(SUSE_OPTS) }
+    let(:node) { runner.node }
+    let(:chef_run) do
+      runner.converge(described_recipe)
+    end
+
+    it 'should install the rabbitmq package' do
+      expect(chef_run).to install_package('rabbitmq-server')
+    end
+
+    it 'should install the rabbitmq plugin package' do
+      expect(chef_run).to install_package('rabbitmq-server-plugins')
+    end
+  end
+
+  describe 'debian-use_distro_version' do
+    let(:runner) { ChefSpec::ServerRunner.new(UBUNTU_OPTS) }
+    let(:node) { runner.node }
+    let(:chef_run) do
+      node.set['rabbitmq']['use_distro_version'] = true
+      runner.converge(described_recipe)
+    end
+
+    it 'should install the logrotate package' do
+      expect(chef_run).to install_package('logrotate')
+    end
+
+    it 'should install the rabbitmq distro package' do
+      expect(chef_run).to install_package('rabbitmq-server')
+    end
+  end
+
+  describe 'debian' do
+    let(:runner) { ChefSpec::ServerRunner.new(UBUNTU_OPTS) }
+    let(:node) { runner.node }
+    let(:chef_run) do
+      node.set['rabbitmq']['version'] = '3.4.2'
+      runner.converge(described_recipe)
+    end
+
+    it 'should install the logrotate package' do
+      expect(chef_run).to install_package('logrotate')
+    end
+
+    it 'creates a rabbitmq-server rpm in the cache path' do
+      expect(chef_run).to create_remote_file_if_missing("#{Chef::Config[:file_cache_path]}/rabbitmq-server_#{node['rabbitmq']['version']}-1_all.deb")
+    end
+
+    it 'installs the rabbitmq-server rpm_package with the default action' do
+      expect(chef_run).to install_dpkg_package("#{Chef::Config[:file_cache_path]}/rabbitmq-server_#{node['rabbitmq']['version']}-1_all.deb")
+    end
+  end
+
+
+  describe 'redhat' do
+    let(:runner) { ChefSpec::ServerRunner.new(REDHAT_OPTS) }
+    let(:node) { runner.node }
+    let(:chef_run) do
+      node.set['rabbitmq']['version'] = '3.4.2'
+      runner.converge(described_recipe)
+    end
+
   it 'creates a rabbitmq-server rpm in the cache path' do
-    expect(chef_run).to create_remote_file_if_missing("#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{version}.noarch.rpm")
+    expect(chef_run).to create_remote_file_if_missing("#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{node['rabbitmq']['version']}-1.noarch.rpm")
   end
 
   it 'installs the rabbitmq-server rpm_package with the default action' do
-    expect(chef_run).to install_rpm_package("#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{version}.noarch.rpm")
+    expect(chef_run).to install_rpm_package("#{Chef::Config[:file_cache_path]}/rabbitmq-server-#{node['rabbitmq']['version']}-1.noarch.rpm")
+  end
+
   end
 
   it 'creates a template rabbitmq.config with attributes' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,29 @@
+# Encoding: utf-8
 require 'chefspec'
 require 'chefspec/berkshelf'
 require 'fauxhai'
 ChefSpec::Coverage.start!
 
-RSpec.configure do |config|
-  config.platform = 'redhat'
-  config.version = '6.5'
-end
+require 'chef/application'
+
+LOG_LEVEL = :fatal
+SUSE_OPTS = {
+  platform: 'suse',
+  version: '11.3',
+  log_level: LOG_LEVEL
+}
+REDHAT_OPTS = {
+    platform: 'redhat',
+    version: '6.5',
+    log_level: LOG_LEVEL
+}
+UBUNTU_OPTS = {
+    platform: 'ubuntu',
+    version: '12.04',
+    log_level: LOG_LEVEL
+}
+CENTOS_OPTS = {
+    platform: 'centos',
+    version: '6.5',
+    log_level: LOG_LEVEL
+}
