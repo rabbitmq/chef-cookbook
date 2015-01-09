25168ae29c1bfb041ef68ab58de2650c8c712f70
Autumn Wang <autumn.wang@autodesk.com>
2015-01-09 06:31:49 -0800

    Removed creat file for plugins

--- recipes/default.rb
+++ recipes/default.rb
@@ -185,13 +185,6 @@ template "#{node['rabbitmq']['config_root']}/rabbitmq.config" do
   notifies :restart, "service[#{node['rabbitmq']['service_name']}]", :immediately
 end
 
-file "#{node['rabbitmq']['config_root']}/enabled_plugins" do
-  owner 'root'
-  group 'root'
-  mode '00644'
-  action :create
-end
-
 
 if File.exist?(node['rabbitmq']['erlang_cookie_path']) && File.readable?((node['rabbitmq']['erlang_cookie_path']))
   existing_erlang_key =  File.read(node['rabbitmq']['erlang_cookie_path']).strip
