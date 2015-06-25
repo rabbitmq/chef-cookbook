6e0357a4b22513e522118b8bcdd2463cce6e3f05
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-06-25 12:56:33 -0700

    Support rabbitmq_user with multiple vhosts. Resolves #278.

--- resources/user.rb
+++ resources/user.rb
@@ -21,7 +21,7 @@ actions :add, :delete, :set_permissions, :clear_permissions, :set_tags, :clear_t
 
 attribute :user, :kind_of => String, :name_attribute => true
 attribute :password, :kind_of => String
-attribute :vhost, :kind_of => String
+attribute :vhost, :kind_of => [String, Array]
 attribute :permissions, :kind_of => String
 attribute :tag, :kind_of => String
 
