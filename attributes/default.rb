91c8ea61f1d58a383f118e5d2be131e56b9c2df7
Dru Goradia <dgoradia@users.noreply.github.com>
2016-07-28 14:17:36 -0700

    Comments out example HA policy attributes (#384)

--- attributes/default.rb
+++ attributes/default.rb
@@ -147,15 +147,15 @@ end
 default['rabbitmq']['heartbeat'] = 580
 
 # Example HA policies
-default['rabbitmq']['policies']['ha-all']['pattern'] = '^(?!amq\\.).*'
-default['rabbitmq']['policies']['ha-all']['params'] = { 'ha-mode' => 'all' }
-default['rabbitmq']['policies']['ha-all']['priority'] = 0
-
-default['rabbitmq']['policies']['ha-two']['pattern'] = '^two.'
-default['rabbitmq']['policies']['ha-two']['params'] = { 'ha-mode' => 'exactly', 'ha-params' => 2 }
-default['rabbitmq']['policies']['ha-two']['priority'] = 1
-
-default['rabbitmq']['disabled_policies'] = []
+# default['rabbitmq']['policies']['ha-all']['pattern'] = '^(?!amq\\.).*'
+# default['rabbitmq']['policies']['ha-all']['params'] = { 'ha-mode' => 'all' }
+# default['rabbitmq']['policies']['ha-all']['priority'] = 0
+#
+# default['rabbitmq']['policies']['ha-two']['pattern'] = '^two.'
+# default['rabbitmq']['policies']['ha-two']['params'] = { 'ha-mode' => 'exactly', 'ha-params' => 2 }
+# default['rabbitmq']['policies']['ha-two']['priority'] = 1
+#
+# default['rabbitmq']['disabled_policies'] = []
 
 # conf
 default['rabbitmq']['conf'] = {}
