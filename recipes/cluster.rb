31b0c5701c3f383eafb6370707bd6156a5dc9ff8
iamthemuffinman <robertjdeusser@gmail.com>
2017-05-15 13:35:34 -0400

    Fix resource cloning deprecation warnings

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -25,21 +25,20 @@ include_recipe 'rabbitmq::default'
 cluster_nodes = node['rabbitmq']['clustering']['cluster_nodes']
 cluster_nodes = cluster_nodes.to_json
 
+auto_cluster_nodes = cluster_nodes
+static_cluster_nodes = cluster_nodes
+
 # Manual clustering
 unless node['rabbitmq']['clustering']['use_auto_clustering']
   # Join in cluster
-  rabbitmq_cluster cluster_nodes do
+  rabbitmq_cluster auto_cluster_nodes do
     cluster_name node['rabbitmq']['clustering']['cluster_name']
     action :join
   end
 end
 
 # Set cluster name : It will be skipped once same cluster name has been set in the cluster.
-rabbitmq_cluster cluster_nodes do
+rabbitmq_cluster static_cluster_nodes do
   cluster_name node['rabbitmq']['clustering']['cluster_name']
-  action :set_cluster_name
-end
-# Change the cluster node type
-rabbitmq_cluster cluster_nodes do
-  action :change_cluster_node_type
+  action [:set_cluster_name, :change_cluster_node_type]
 end
