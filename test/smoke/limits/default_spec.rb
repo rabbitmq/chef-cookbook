7b414fc59cde526319d3344875851db1cc123252
Michael Klishin <michael@clojurewerkz.org>
2019-03-05 03:45:13 +0300

    Commit the new test suite file
    (cherry picked from commit c30d1d09edb2544081cf38e0c4e66bbc3f3240a8)

---
+++ test/smoke/limits/default_spec.rb
@@ -0,0 +1,27 @@
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe package('rabbitmq-server') do
+  it { should be_installed }
+end
+
+describe service('rabbitmq-server') do
+  it { should be_running }
+end
+
+describe command('HOSTNAME=$(hostname) rabbitmq-diagnostics ping') do
+  its(:exit_status) { should eq 0 }
+end
+describe file('/etc/systemd/system/rabbitmq-server.service.d/limits.conf') do
+  it { should be_file }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+
+  its('content') { should match(%r{LimitNOFILE=54000}) }
+end
+
+describe file('/etc/rabbitmq/rabbitmq.config') do
+  it { should be_file }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+end
