c3e84b91c686d3265bdc5e4935a51b8a58a878bf
Michael Klishin <michael@clojurewerkz.org>
2019-03-12 17:54:11 +0300

    LWRPs 3.6.x test: more assertions, references #508

--- test/smoke/36x_lwrps/default_spec.rb
+++ test/smoke/36x_lwrps/default_spec.rb
@@ -1,6 +1,18 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
+describe package('rabbitmq-server') do
+  it { should be_installed }
+end
+
+describe service('rabbitmq-server') do
+  it { should be_running }
+end
+
+describe command('HOSTNAME=$(hostname) rabbitmq-plugins list') do
+  its(:exit_status) { should eq 0 }
+end
+
 describe command('curl -u guest:guest -H "Accept: application/json" -X GET "http://localhost:15672/api/overview"') do
   its(:exit_status) { should eq 0 }
 end
