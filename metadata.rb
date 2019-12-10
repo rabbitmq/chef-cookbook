b2f5a0bbf5d11f624ee838442897a06ff8b8e943
Tim Smith <tsmith84@gmail.com>
2019-12-10 10:45:17 -0800

    Remove recipe data from the metadata
    This is unused and there's no point in including it here
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,29 +6,6 @@ license 'Apache-2.0'
 description 'Installs and configures RabbitMQ server'
 version '6.0.0'
 
-recipe 'rabbitmq', 'Install and configure RabbitMQ'
-
-recipe 'rabbitmq::systemd_limits', 'Sets up kernel limits (e.g. nofile) for RabbitMQ via systemd'
-recipe 'rabbitmq::cluster', 'Set up RabbitMQ clustering.'
-
-recipe 'rabbitmq::management_ui', 'Sets up RabbitMQ management plugin/UI'
-recipe 'rabbitmq::mgmt_console', 'Deprecated, alias for rabbitmq::management_ui'
-
-recipe 'rabbitmq::plugins', 'Manage plugins with node attributes'
-recipe 'rabbitmq::plugin_management', 'Deprecated, alias for rabbitmq::plugins'
-
-recipe 'rabbitmq::vhosts', 'Manage virtual hosts with node attributes'
-recipe 'rabbitmq::virtualhost_management', 'Deprecated, alias for rabbitmq::vhosts'
-
-recipe 'rabbitmq::users', 'Manage users with node attributes'
-recipe 'rabbitmq::user_management', 'Deprecated, alias for rabbitmq::users'
-
-recipe 'rabbitmq::policies', 'Manage policies with node attributes'
-recipe 'rabbitmq::policy_management', 'Deprecated, alias for rabbitmq::policies'
-
-recipe 'rabbitmq::erlang_package', 'Provisions Erlang via Team RabbitMQ packages'
-recipe 'rabbitmq::esl_erlang_package', 'Alias for erlang::esl'
-
 issues_url 'https://github.com/rabbitmq/chef-cookbook/issues'
 source_url 'https://github.com/rabbitmq/chef-cookbook'
 
